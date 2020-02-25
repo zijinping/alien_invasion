3005a33ddc0b2d6b1f9125081ec7a517f6f37b4c
zijinping <zijinping@qq.com>
2020-02-25 17:10:35 +0800

    Delete settings.py

--- settings.py
+++
@@ -1,7 +0,0 @@
-class Settings():
-	#settings for initialize the game
-	def __init__(self):
-		#settings information
-		self.screen_width = 1200
-		self.screen_height = 800
-		self.bg_color = (230, 230, 230)
